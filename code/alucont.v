92cff43027643304119f6eaa77a529a9e1aa2f3d
XeniaPhe <m.tunahanbas90@gmail.com>
2023-06-03 19:35:28 +0300

    add jmor hopefully without bugs

--- code/alucont.v
+++ code/alucont.v
@@ -8,12 +8,12 @@ if(~(aluop1|aluop0))  gout=3'b010;
 if(aluop0)gout=3'b110;
 if(aluop1)//R-type
 begin
-	if (~(f3|f2|f1|f0))gout=3'b010; 	//function code=0000,ALU control=010 (add)
-	if (f1&f3)gout=3'b111;			//function code=1x1x,ALU control=111 (set on less than)
-	if (f1&~(f3))gout=3'b110;		//function code=0x10,ALU control=110 (sub)
-	if (f2&f0)gout=3'b001;			//function code=x1x1,ALU control=001 (or)
-	if (f2&~(f0))gout=3'b000;		//function code=x1x0,ALU control=000 (and)
-	if (~(f5)&f4&~(f3)&f2&f1&~(f0))gout=3'b011; //TODO: funct code=010110?? function code=x0111, ALU control = 011 (balrz)
+	if (~(f3|f2|f1|f0))gout=3'b010; 			//function code=0000,ALU control=010 (add)
+	if (f1&f3)gout=3'b111;						//function code=1x1x,ALU control=111 (set on less than)
+	if (f1&~(f3))gout=3'b110;					//function code=0x10,ALU control=110 (sub)
+	if (f2&f0)gout=3'b001;						//function code=x1x1,ALU control=001 (or)
+	if (f2&~(f0))gout=3'b000;					//function code=x1x0,ALU control=000 (and)
+	if (~(f5)&f4&~(f3)&f2&f1&~(f0))gout=3'b011; //function code=010110, ALU control = 011 (register1)
 end 
 end
 endmodule
--- code/control.v
+++ code/control.v
@@ -1,7 +1,20 @@
-module control(in,regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop1,aluop2);
-input [5:0] in;
-output balrz, regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop1,aluop2;
+module control(
+    input [5:0] in,
+    input [5:0] funct,
+    output regdest,
+    output alusrc,
+    output jump,
+    output memtoreg,
+    output regtoreg,
+    output regwrite,
+    output memread,
+    output memwrite,
+    output branch,
+    output aluop1,
+    output aluop2);
+
 wire rformat,lw,sw,beq;
+
 assign rformat=~|in;
 assign lw=in[5]& (~in[4])&(~in[3])&(~in[2])&in[1]&in[0];
 assign sw=in[5]& (~in[4])&in[3]&(~in[2])&in[1]&in[0];
@@ -15,5 +28,7 @@ assign memwrite=sw;
 assign branch=beq;
 assign aluop1=rformat;
 assign aluop2=beq;
-assign balrz=rformat; //TODO: check this
+assign regtoreg = rformat & (~funct[5]) & funct[4] & (~funct[3] & funct[2] & funct[1] & (~funct[0]));
+assign jump = rformat & (funct[5] & (~funct[4]) & (funct[3]) & funct[2] & (~funct[1]) & funct[0]);
+
 endmodule
