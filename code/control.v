0d7f706c61336ad021f61f8775865865569c3f4d
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-06-04 18:35:27 +0300

    I really hope eveything is fine cuz I am not

--- code/control.v
+++ code/control.v
@@ -3,6 +3,7 @@ module control(
     input [5:0] funct,
     output regdest,
     output alusrc,
+    output shift,
     output jz,
     output js,
     output jmem,
@@ -22,10 +23,10 @@ assign rformat=~|in;
 assign lw=in[5]& (~in[4])&(~in[3])&(~in[2])&in[1]&in[0];
 assign sw=in[5]& (~in[4])&in[3]&(~in[2])&in[1]&in[0];
 assign beq=~in[5]& (~in[4])&(~in[3])&in[2]&(~in[1])&(~in[0]);
-assign regdest=rformat;
+assign regdest=rformat|shift;
 assign alusrc=lw|sw|bmem;
 assign memtoreg=lw;
-assign regwrite=rformat|lw;
+assign regwrite=rformat|lw|shift;
 assign memread=lw|bmem|jmem|js;
 assign memwrite=sw|js;
 assign branch=beq;
@@ -36,5 +37,5 @@ assign jmem = rformat & (funct[5] & (~funct[4]) & (funct[3]) & funct[2] & (~func
 assign bmem = (~in[5]) & in[4] & (~in[3]) & in[2] & (~in[1]) & (~in[0]);
 assign js = (~in[5]) & in[4] & (~in[3]) & (~in[2]) & in[1] & in[0];
 assign jz = (~in[5]) & in[4] & in[3] & (~in[2]) & (in[1]) & (~in[0]);
-
+assign shift = rformat & (~funct[5]) & (~funct[4]) & (~funct[3]) & funct[2] & (~funct[1]) & (~funct[0]);
 endmodule
